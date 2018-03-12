a505d0c8176c9ef74b2dde5bf2b472f062522045
Borja Cano <borcano@ucm.es>
2018-03-12 18:32:21 +0100

    Añadidas dependencias con bullet a la solución

--- Pprueba/Src/InputManager.cpp
+++ Pprueba/Src/InputManager.cpp
@@ -17,26 +17,3 @@ void InputManager::update()
 {
 
 }
-
-
-void InputManager::subscribe(INPUT_NOTIFICATION notification, std::function<void> callback)
-{
-	if (!subscribers.count(notification))
-		subscribers.emplace(notification);
-
-	subscribers[notification].push_back(callback);
-}
-
-void InputManager::unsubscribe(INPUT_NOTIFICATION notification, std::function<void> callback)
-{
-}
-
-void InputManager::parseEvents()
-{
-	//Traducir los eventos del sistema a los que a nosotros nos interesan
-}
-
-void InputManager::dispachNotificacations()
-{
-	//hacer las llamadas a los callbacks pertinentes
-}
--- Pprueba/Src/InputManager.h
+++ Pprueba/Src/InputManager.h
@@ -1,19 +1,7 @@
 #ifndef _H_INPUTMANAGER_H_
 #define _H_INPUTMANAGER_H_
 
-#include <map>
-#include <deque>
-#include <functional>
 class Game;
-
-enum class INPUT_NOTIFICATION : unsigned int
-{
-	MOUSE_OVER,
-	LEFT_CLICK,
-	RIGHT_CLICK,
-	A_PRESSED,
-	D_PRESSED,
-};
 
 class InputManager
 {
@@ -23,18 +11,6 @@ public:
 
 	void update();
 
-
-	void subscribe(INPUT_NOTIFICATION notification, std::function<void> callback);
-	void unsubscribe(INPUT_NOTIFICATION notification, std::function<void> callback);
-
-
-private:
-	void parseEvents();
-	void dispachNotificacations();
-
-private:
-	std::deque <INPUT_NOTIFICATION> notifications;
-	std::map < INPUT_NOTIFICATION, std::vector<std::function<void>>> subscribers;
 };
 #endif //!_H_INPUTMANAGER_H_
 
