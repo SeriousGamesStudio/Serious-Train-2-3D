2ed284b721ace017dd7d7c714407b45f8223fe7a
Usuario Local <Usuario Local>
2018-05-23 13:06:48 +0200

    Objetos mirando donde deberían

--- Pprueba/Src/MeshRenderer_c.cpp
+++ Pprueba/Src/MeshRenderer_c.cpp
@@ -7,6 +7,7 @@ MeshRenderer_c::MeshRenderer_c(std::string meshName, float scale) :
 {
 	meshPath = meshName;
 	this->scale = scale;
+
 }
 
 MeshRenderer_c::~MeshRenderer_c()
@@ -17,9 +18,12 @@ void MeshRenderer_c::start()
 {
 	scnNode_ = GraphicsManager::getInstance()->createNewNode(meshPath, _myEntity->getName());
 	scnNode_->scale(scale, scale, scale);
-	static_cast<Transform_c*>(getComponent(ComponentType::TRANSFORM))->get().getRotation();
+	btQuaternion rot = static_cast<Transform_c*>(getComponent(ComponentType::TRANSFORM))->get().getRotation();
 	//Hay que rotar el nodo  con el transform que hemos cogido
 
+	Ogre::Quaternion conQuat(rot.w(), rot.x(), rot.y(), rot.z());
+	//Ogre::Vector3 newDir = conQuat * Ogre::Vector3::UNIT_X;
+	scnNode_->rotate(conQuat);
 }
 void MeshRenderer_c::listen(Msg_Base* msg)
 {
